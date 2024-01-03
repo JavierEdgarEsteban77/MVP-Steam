955c6e4a99f7c6c13e6a257aaee693c37cac4207
Javier Edgar <86074229+JavierEdgarEsteban77@users.noreply.github.com>
2024-01-02 21:09:39 -0300

    Actualización

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# Importo librerías.
 import fastapi
 import pandas as pd
 import numpy as np
@@ -8,44 +9,24 @@ from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import nltk
 nltk.download('vader_lexicon')
 
+# Carga los dataframes desde los archivos CSV.
+df_users_items = pd.read_csv('C:\\Users\\Esteban García\\OneDrive\\Escritorio\\MVP Steam\\Data\\users_items.csv')
+df_user_reviews = pd.read_csv('C:\\Users\\Esteban García\\OneDrive\\Escritorio\\MVP Steam\\Data\\user_reviews.csv')
+
 # Función para realizar análisis de sentimientos
 def sentiment_analysis(review):
-    """Realiza un análisis de sentimientos en una reseña.
-
-    Args:
-        review (str): La reseña a analizar.
-
-    Returns:
-        float: El puntaje de sentimiento de la reseña.
-    """
     sid = SentimentIntensityAnalyzer()
     sentiment_scores = sid.polarity_scores(review)
     return sentiment_scores['compound']
 
 # Función para obtener el año de lanzamiento con más horas jugadas para un género específico
 def PlayTimeGenre(genero: str):
-    """Obtiene el año de lanzamiento con más horas jugadas para un género específico.
-
-    Args:
-        genero (str): El género de los juegos.
-
-    Returns:
-        dict: Un diccionario con el año de lanzamiento con más horas jugadas para el género dado.
-    """
     df_genre = df_users_items[df_users_items['genero'] == genero]
     year_most_played = df_genre.groupby('año')['playtime_forever'].sum().idxmax()
     return {"Año de lanzamiento con más horas jugadas para Género X" : year_most_played}
 
 # Función para obtener el usuario con más horas jugadas para un género específico
 def UserForGenre(genero: str):
-    """Obtiene el usuario con más horas jugadas para un género específico.
-
-    Args:
-        genero (str): El género de los juegos.
-
-    Returns:
-        dict: Un diccionario con el usuario que tiene más horas jugadas para el género dado y las horas jugadas por año.
-    """
     df_genre = df_users_items[df_users_items['genero'] == genero]
     user_most_played = df_genre.groupby('user_id')['playtime_forever'].sum().idxmax()
     hours_played_per_year = df_genre[df_genre['user_id'] == user_most_played].groupby('año')['playtime_forever'].sum().to_dict()
@@ -53,14 +34,6 @@ def UserForGenre(genero: str):
 
 # Función para obtener los juegos más recomendados en un año específico
 def UsersRecommend(año: int):
-    """Obtiene los juegos más recomendados en un año específico.
-
-    Args:
-        año (int): El año de las recomendaciones.
-
-    Returns:
-        list: Una lista con los tres juegos más recomendados en el año dado.
-    """
     df_year = df_user_reviews[df_user_reviews['año'] == año]
     df_recommended = df_year[df_year['recommend'] == True]
     top_games = df_recommended['item_name'].value_counts().nlargest(3).index.tolist()
@@ -68,14 +41,6 @@ def UsersRecommend(año: int):
 
 # Función para obtener los juegos menos recomendados en un año específico
 def UsersNotRecommend(año: int):
-    """Obtiene los juegos menos recomendados en un año específico.
-
-    Args:
-        año (int): El año de las recomendaciones.
-
-    Returns:
-        list: Una lista con los tres juegos menos recomendados en el año dado.
-    """
     df_year = df_user_reviews[df_user_reviews['año'] == año]
     df_not_recommended = df_year[df_year['recommend'] == False]
     top_games = df_not_recommended['item_name'].value_counts().nlargest(3).index.tolist()
@@ -83,24 +48,11 @@ def UsersNotRecommend(año: int):
 
 # Función para obtener el análisis de sentimientos de las reseñas en un año específico
 def sentiment_analysis_year(año: int):
-    """Obtiene el análisis de sentimientos de las reseñas en un año específico.
-
-    Args:
-        año (int): El año de las reseñas.
-
-    Returns:
-        dict: Un diccionario con la cantidad de reseñas negativas, neutrales y positivas en el año dado.
-    """
     df_year = df_user_reviews[df_user_reviews['año'] == año]
     sentiments = df_year['sentiment_analysis'].value_counts().to_dict()
     return sentiments
 
-app = fastapi.FastAPI(title="MVP Steam PI ML OPs",
-              description="Descripción: Obtener el Producto Mínimo Viable",
-              contact={"name": "Javier Edgar Esteban",
-                       "url": "https://github.com/JavierEdgarEsteban77/MVP-Steam",
-                       "email": "javieredgaresteban@gmail.com",
-                       "Tel.": "+5492612543003383"})
+app = fastapi.FastAPI(title="MVP Steam PI ML OPs")
 
 @app.get("/PlayTimeGenre/{genero}")
 def get_PlayTimeGenre(genero: str):
@@ -123,4 +75,4 @@ def get_sentiment_analysis_year(año: int):
     return sentiment_analysis_year(año)
 
 if __name__ == '__main__':
-    uvicorn.run("mean:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
